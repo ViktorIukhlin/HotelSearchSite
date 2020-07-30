67ded5b4b96867bb1cf09e4f29040fe783afa0de
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-07-31 00:53:29 +0300

    add listener

--- src/pages/UI kit/calendar/src/js/script.js
+++ src/pages/UI kit/calendar/src/js/script.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const parentDays = document.querySelector('.calendar__daysOftheMonth');
+const parentDays = document.querySelector('.calendar__daysOftheMonth'),
+      numbers = document.querySelectorAll('.calendar__daysOftheMonth');
 
 for(let i = 0; i < 35; i++){
     parentDays.innerHTML+=`<div class="calendar__number">${i-2}</div>`;
 }
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        e.target.classList.toggle('calendar__number_active');
+    });
+
+});
