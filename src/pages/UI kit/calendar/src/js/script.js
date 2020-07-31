4e4c528ae4be98031c2f1bfb0d2631ba54011e32
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-07-31 14:50:56 +0300

    ++

--- src/pages/UI kit/calendar/src/js/script.js
+++ src/pages/UI kit/calendar/src/js/script.js
@@ -1,15 +1,37 @@
 'use strict';
 
 const parentDays = document.querySelector('.calendar__daysOftheMonth'),
-      numbers = document.querySelectorAll('.calendar__daysOftheMonth');
+    numbers = document.querySelectorAll('.calendar__daysOftheMonth'),
+    clear = document.querySelector('.calendar__clear'),
+    apply = document.querySelector('.calendar__sumbit');
 
-for(let i = 0; i < 35; i++){
-    parentDays.innerHTML+=`<div class="calendar__number">${i-2}</div>`;
+const data = new Date();
+
+let dataNow = data.getDate();
+
+while(dataNow >= 1){
+    console.log(dataNow);
+    dataNow--;
 }
 
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        e.target.classList.toggle('calendar__number_active');
-    });
+for (let i = 0; i < 35; i++) {
+    parentDays.innerHTML += `<div data-day class="calendar__number">${i - 2}</div>`;
+}
+const days = document.querySelectorAll('[data-day]');
 
+
+days.forEach(day => {
+    day.addEventListener('click', () => {
+        day.classList.toggle('calendar__number_active'); 
+    });
+});
+
+clear.addEventListener('click', () => {
+    days.forEach(day => {
+        day.classList.remove("calendar__number_active");
+    });
+});
+
+apply.addEventListener('click', () => {
+        console.log(`Hello`);    
 });
